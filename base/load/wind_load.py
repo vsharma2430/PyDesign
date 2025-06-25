366610198ee742ddb6f20bada9ce63bb4fd06bfa
Vaibhav Sharma <59431005+vsharma2430@users.noreply.github.com>
2025-06-25 15:29:27 +0530

    windload pkl separated

--- base/load/wind_load.py
+++ base/load/wind_load.py
@@ -1,3 +1,4 @@
+import pickle
 from copy import deepcopy
 
 class WindLoad:
@@ -20,3 +21,26 @@ class WindLoad:
         return load
     
 add_elevation_to_wind_load_list_fn = lambda ground_level : lambda loads : list(map(lambda load: load.add_elevation(ground_level), [*loads]))
+
+def export_windload_pickle(load_object,filename):
+    """
+    Exports the windload object object to a pickle file.
+    
+    Args:
+        filename (str): The name of the file to save the pickle object to.
+    """
+    with open(filename, 'wb') as file:
+        pickle.dump(load_object, file)
+
+def import_windload_pickle(filename):
+    """
+    Loads a Windload object from a pickle file.
+    
+    Args:
+        filename (str): The name of the file to read the pickle object from.
+        
+    Returns:
+        Piperack: The deserialized Piperack object.
+    """
+    with open(filename, 'rb') as file:
+        return pickle.load(file)
